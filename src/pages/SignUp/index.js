0e4c73444845a262961c422452f8dd4bbe3b456c
Leonardo Vitale <leonardo.vitale@outlook.com>
2019-09-25 20:02:10 +0200

    feat: validation with Yup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 import { Form, Input } from '@rocketseat/unform';
 import logo from 'assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('O nome é obrigatória'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
   const handleSubmit = data => {
@@ -11,7 +22,7 @@ export default function SignUp() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Senha" />
